48c00f62c8d325c5a92c4f77e10c0f9556a232b6
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-05-20 19:38:05 +0900

    feat: 사진 라이브러리 수정 작업 중
    사진 라이브러리 수정 작업 중

--- EmergencyApp/View/SecondView/View/PhotoLibraryView.swift
+++ EmergencyApp/View/SecondView/View/PhotoLibraryView.swift
@@ -115,16 +115,35 @@ struct PhotoLibraryView: View {
  
         }
     }
-    func loadImage() {
-        guard let inputImage = inputImage else {
-            return
-        }
-        image = Image(uiImage: inputImage)
+//    func loadImage() {
+//        guard let inputImage = inputImage else {
+//            return
+//        }
+//        image = Image(uiImage: inputImage)
+//
+//    }
+//
+//    func save() {
+//
+//    }
+}
 
+extension URL {
+    func loadImage(_ image: inout UIImage?) {
+        if let data = try? Data(contentsOf: self), let loaded = UIImage(data: data) {
+            image = loaded
+        } else {
+            image = nil
+        }
     }
-    
-    func save() {
-        
+    func saveImage(_ image: UIImage?) {
+        if let image = image {
+            if let data = image.jpegData(compressionQuality: 1.0) {
+                try? data.write(to: self)
+            }
+        } else {
+            try? FileManager.default.removeItem(at: self)
+        }
     }
 }
         
